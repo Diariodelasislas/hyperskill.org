adf45ae42b7c1b4a6c6196940a215f0e5c5899bd
Diariodelasislas <119725494+Diariodelasislas@users.noreply.github.com>
2023-02-21 18:53:28 +0800

    Delete SimpleBot1.0.java

--- Java/bot/SimpleBot1.0.java
+++
@@ -1,9 +0,0 @@
-package bot;
-
-public class SimpleBot {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello! My name is Diariodelasislas.");
-        System.out.println("I was created in 2023.");
-    }
-}
