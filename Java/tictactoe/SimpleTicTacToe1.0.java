a5aa5f540c50b40385e229a7dcc08473ab7b89f3
JiaxingLi <iceman_1404@hotmail.com>
2023-02-23 23:35:20 +0800

    test

--- Java/tictactoe/SimpleTicTacToe1.0.java
+++
@@ -1,8 +0,0 @@
-package tictactoe;
-
-public class SimpleTicTacToe {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("X O X\nO X O\nX X O");
-    }
-}
